efe0a36166a997081b54d4f3dc21c12d944a7e01
João Pedro <jpappelt10@gmail.com>
2024-11-12 19:40:09 -0300

    Add files via upload

---
+++ 28-10/EscritaArray.js
@@ -0,0 +1,46 @@
+// 1° Exercicio de escrita
+/*const readline = require(`readline-sync`)
+const nome = readline.question("Qual seu nome ?")
+const email = readline.question("Qual seu email ?")
+console.log(`O email ${email} foi cadastrado com sucesso. Seja bem vindo ${nome}`)*/
+
+// 2° Exercicio de escrita
+/*const comidaFavoritas = ["Pizza","Lasanha","Açai","Hamburguer","Churrasco"]
+console.log(comidaFavoritas)
+console.log("Essa são as minhas comidas favoritas")
+for(let i = 0; i < comidaFavoritas.length; i++)
+    console.log(comidaFavoritas[i])
+
+// Desafio
+
+const readline = require(`readline-sync`)
+const comidaFav = readline.question("Qual sua comida favorita?")
+comidaFavoritas[1] = comidaFav
+console.log(comidaFavoritas)*/
+
+
+//3° Escrita de escrita
+
+/*const listaDeTarefas = []
+const readline = require(`readline-sync`)
+const tarefas1 = readline.question("Fale 3 tarefas voce faz no seu dia a dia")
+const tarefas2 = readline.question("Fale outra tarefa voce faz no seu dia a dia")
+const tarefas3 = readline.question("Fale a ultima tarefas voce faz no seu dia a dia")
+listaDeTarefas.push(tarefas1)
+listaDeTarefas.push(tarefas2)
+listaDeTarefas.push(tarefas3)
+console.log(listaDeTarefas)
+const escolha = readline.questionInt("Escolha de 1 a 3")
+if(escolha >= 1 && escolha <= 3){
+    listaDeTarefas.splice(escolha - 1, 1)
+}else{
+    console.log("Esse numero não existe")
+}
+console.log(listaDeTarefas)*/
+
+
+
+
+
+//3° Escrita de escrita
+
---
+++ 28-10/ExerciciosArray3.js
@@ -0,0 +1,7 @@
+const number = [1, 2 ,3 ,4, 5, 6]
+console.log(number.length)
+number.push(7)
+console.log(number)
+number.splice(3,2)
+console.log(number)
+console.log(number.length)
