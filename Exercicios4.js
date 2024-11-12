7f67d19c39297317a4c03b7c74931a05773b6f3c
João Pedro <jpappelt10@gmail.com>
2024-11-12 19:29:46 -0300

    Delete Exercicios4.js

--- Exercicios4.js
+++
@@ -1,5 +0,0 @@
-function calculaFahrenheit(temperaturaCelsius){
-    const temperaturaFahnhereit = (temperaturaCelsius * 1.8) + 32
-    return temperaturaFahnhereit
-}
-console.log(calculaFahrenheit(20))
