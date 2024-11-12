480a5a4fd81dfeb47b8d021a371b04f48066de70
João Pedro <jpappelt10@gmail.com>
2024-11-12 19:28:20 -0300

    Delete Algo-complexo.js

--- Algo-complexo.js
+++
@@ -1,8 +0,0 @@
-function calculaFahrenheit(temperaturaCelsius){
-    const temperaturaFahnhereit = (temperaturaCelsius * 1.8) + 32
-    return {temperaturaFahnhereit,temperaturaCelsius}
-}
-const {temperaturaFahnhereit,temperaturaCelsius} = calculaFahrenheit(30)
-
-console.log(temperaturaFahnhereit)
-console.log(temperaturaCelsius)
