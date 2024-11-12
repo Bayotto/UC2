31781de301240e207e999dd33971823f5687b0f4
João Pedro <jpappelt10@gmail.com>
2024-11-12 19:39:49 -0300

    Delete 28-10 directory

--- 28-10/ExerciciosArray2.js
+++
@@ -1,6 +0,0 @@
-const readline = require(`readline-sync`)
-const Raças = ["Dogo argentino","Rottweiler","Pastor caucasiano","Husky siberiano","Doberman."]
-const indice = readline.questionInt( "Escreva uma raça: /n1 - Labrador /n2 - Vira-Lata /n3 - Golden-Retriever /n4 - Bulldog /n5 - Huskie /n")
-
-console.log("Você escolheu:", Raças[indice - 1])
-
