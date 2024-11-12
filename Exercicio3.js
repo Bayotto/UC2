eccd7e0ac54f88ab090e412175114ab1b36e999e
João Pedro <jpappelt10@gmail.com>
2024-11-12 19:31:25 -0300

    Delete Exercicio3.js

--- Exercicio3.js
+++
@@ -1,7 +0,0 @@
- const readline = require(`readline-sync`)
-const number1 = Number(readline.questionInt("Escolha um numero para fazer a tabuada"))
-let tabuada = 0
-while (tabuada <= 10){
-console.log(`${tabuada} x ${number1} = ${tabuada * number1}`)
-tabuada++
-}
