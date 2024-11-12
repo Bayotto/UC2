641f7930868310ab15c9938c8fa55340f7d088e6
João Pedro <jpappelt10@gmail.com>
2024-11-12 19:33:23 -0300

    Delete Array.js

--- Array.js
+++
@@ -1,9 +0,0 @@
-const array = ["agua","arroz","feijao","tortinha"]
-console.log(array)
-array.push("Refri")
-console.log(array)
-array.pop()
-console.log(array)
-console.log(array[0])
-console.log(array[1])
-console.log(array[2])
